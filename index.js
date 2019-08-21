26d5a3c5f36a876ab7e21fc54817435e5937f910
Nate Tan <nktan93@gmail.com>
2019-08-21 19:47:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(array, choice) {
-  array.filter(function (name) { return name.toLowerCase() === choice.toLowerCase(); });
+  return array.filter(function (name) { return name.toLowerCase() === choice.toLowerCase(); });
 }

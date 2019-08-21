66b70b532d4d31972be5c9f8401b9970ddaa5bea
Nate Tan <nktan93@gmail.com>
2019-08-21 19:58:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function findMatching(array, choice) {
   return array.filter(function (name) { return name.toLowerCase() === choice.toLowerCase(); });
 }
+
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+}
+
